09cd282162b93c81c7ed50c8599a7beebefd6c6d
ahadjawaid <ahadjawaid0@gmail.com>
2025-01-01 21:19:16 -0600

    test: added different date sample data

--- Notes/Model/Note.swift
+++ Notes/Model/Note.swift
@@ -34,9 +34,15 @@ class Note {
         return lines.count > 1 ? String(lines[1]) : "No additional text"
     }
     
+    
     static let sampleData = [
         Note(folder: Folder.sampleData[0], body: "Hi I'm bob"),
-        Note(folder: Folder.sampleData[0], body: "I like turtles"),
+        Note(folder: Folder.sampleData[0], body: "I like turtles", lastModified: Calendar.current.date(byAdding: .day, value: -1, to: Date())!),
+        Note(folder: Folder.sampleData[0], body: "I don't like turtles", lastModified: Calendar.current.date(byAdding: .day, value: -3, to: Date())!),
+        Note(folder: Folder.sampleData[0], body: "I like don't to eat", lastModified: Calendar.current.date(byAdding: .day, value: -15, to: Date())!),
+        Note(folder: Folder.sampleData[0], body: "I hate yummy food", lastModified: Calendar.current.date(byAdding: .year, value: -1, to: Date())!),
+        Note(folder: Folder.sampleData[0], body: "This food is yummy", lastModified: Calendar.current.date(byAdding: .year, value: -2, to: Date())!),
+        Note(folder: Folder.sampleData[0], body: "I like water", lastModified: Calendar.current.date(byAdding: .year, value: -3, to: Date())!),
         Note(folder: Folder.sampleData[1], body: "I like to eat"),
     ]
 }
